603ad5cb9e3520c76b86df5d5d675798dd682aa5
Eduardo Amaral <eduamaralmp@gmail.com>
2025-05-06 23:36:36 -0300

    CICD-VERIFICAÇÃO-BRANCH

--- scripts/validate_pr.py
+++ scripts/validate_pr.py
@@ -2,26 +2,26 @@ import os
 
 def validate_branch_and_pr_title(branch, pr_title):
     """
-    Validates if the branch name and PR title follow the 'TADocs-' pattern.
+    Validates if the branch name and PR title follow the 'ATOM-UTILS-' pattern.
     Args:
         branch (str): The name of the branch to validate.
         pr_title (str): The title of the pull request (PR) to validate.
     Returns:
-        bool: True if both the branch and the pull request follow the 'TADocs-' pattern, False otherwise.
+        bool: True if both the branch and the pull request follow the 'ATOM-UTILS-' pattern, False otherwise.
     Prints:
         str: A message indicating whether the branch and PR title are valid or specifying the validation error.
     """
 
-    # Verify if the branch starts with 'TADocs-'
-    if not branch.startswith("TADocs-"):
-        print(f"The branch '{branch}' does not start with the pattern 'TADocs-'.")
+    # Verify if the branch starts with 'ATOM-UTILS-'
+    if not branch.startswith("ATOM-UTILS-"):
+        print(f"The branch '{branch}' does not start with the pattern 'ATOM-UTILS-'.")
         return False
-    # Verify if the branch contains with 'TADocs-'
-    if "TADocs-" not in pr_title:
-        print(f"The PR title '{pr_title}' does not contain the pattern 'TADocs-'.")
+    # Verify if the branch contains with 'ATOM-UTILS-'
+    if "ATOM-UTILS-" not in pr_title:
+        print(f"The PR title '{pr_title}' does not contain the pattern 'ATOM-UTILS-'.")
         return False
     
-    print(f"Validation successful: The branch '{branch}' and the PR title '{pr_title}' follow the 'TADocs-' pattern.")
+    print(f"Validation successful: The branch '{branch}' and the PR title '{pr_title}' follow the 'ATOM-UTILS-' pattern.")
 
     return True
 
